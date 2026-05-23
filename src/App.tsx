3c8d106a21f032215fbf2052d271628a144c8be6
Pipotka <33nasrudin@gmail.com>
2026-05-24 02:17:28 +0300

    fix: hide Sidebar on auth pages (login, register)

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { BrowserRouter, Routes, Route, useLocation } from 'react-router-dom';
 import { Sidebar } from '@/components/Sidebar/Sidebar';
 import { CategoryPage } from '@/pages/CategoryPage/CategoryPage';
 import { EditWalletPage } from '@/pages/EditWalletPage/EditWalletPage';
@@ -11,23 +11,32 @@ import { ProfilePage } from '@/pages/ProfilePage/ProfilePage';
 import { ChangePasswordPage } from '@/pages/ChangePasswordPage/ChangePasswordPage';
 import styles from './App.module.css';
 
+function AppContent() {
+  const location = useLocation();
+  const isAuthPage = location.pathname === '/login' || location.pathname === '/register';
+
+  return (
+    <div className={styles.app}>
+      {!isAuthPage && <Sidebar />}
+      <Routes>
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/register" element={<RegisterPage />} />
+        <Route path="/" element={<CategoryPage />} />
+        <Route path="/wallet/:id" element={<EditWalletPage />} />
+        <Route path="/category/:id" element={<EditCategoryPage />} />
+        <Route path="/transactions/add" element={<TransactionAddPage />} />
+        <Route path="/transactions" element={<TransactionPage />} />
+        <Route path="/profile" element={<ProfilePage />} />
+        <Route path="/profile/change-password" element={<ChangePasswordPage />} />
+      </Routes>
+    </div>
+  );
+}
+
 function App() {
   return (
     <BrowserRouter>
-      <div className={styles.app}>
-        <Sidebar />
-        <Routes>
-          <Route path="/login" element={<LoginPage />} />
-          <Route path="/register" element={<RegisterPage />} />
-          <Route path="/" element={<CategoryPage />} />
-          <Route path="/wallet/:id" element={<EditWalletPage />} />
-          <Route path="/category/:id" element={<EditCategoryPage />} />
-          <Route path="/transactions/add" element={<TransactionAddPage />} />
-          <Route path="/transactions" element={<TransactionPage />} />
-          <Route path="/profile" element={<ProfilePage />} />
-          <Route path="/profile/change-password" element={<ChangePasswordPage />} />
-        </Routes>
-      </div>
+      <AppContent />
     </BrowserRouter>
   );
 }
